362a2d98991c419f20f3d0a35d73fd06e0147164
zfy2299 <zfy2299@qq.com>
2022-07-27 21:04:50 +0800

    init

---
+++ links.js
@@ -0,0 +1,86 @@
+const magnetLinks = [
+  {
+    name: 'FSDSS-424',
+    img: 'https://www.98tuch.net/tupian/forum/202206/23/124446k1dveddbttt7jzzi.jpg',
+    magnet: ['magnet:?xt=urn:btih:B3ED6F4C83DD32A3E181BE1E331CBB865DC3E190'],
+    key: '時田亜美'
+  },
+  {
+    name: 'HODV-21662',
+    img: '',
+    magnet: ['magnet:?xt=urn:btih:780B3BD6A79E8E27CBF2E8AB510A2D2CF95384F2'],
+    key: 'たちばな日菜'
+  },
+  {
+    name: 'midv-136',
+    img: 'https://www.98tuch.net/tupian/forum/202207/01/175945zht4l549dtrd7r79.jpg',
+    magnet: ['magnet:?xt=urn:btih:490A2A79259E0B35FB43E236569847513AE9D2AE'],
+    key: '森千里'
+  },
+  {
+    name: 'stars-575',
+    img: 'https://www.98tuch.net/tupian/forum/202206/14/132556sppxvwxvhscsppxz.jpg',
+    magnet: ['magnet:?xt=urn:btih:053F806DC60B16C1987FCCDEED63B5745814D972'],
+    key: '西元めいさ'
+  },
+  {
+    name: 'STARS-585',
+    img: 'https://www.98tuch.net/tupian/forum/202207/05/134834n72npy17xknbp2d6.jpg',
+    magnet: ['magnet:?xt=urn:btih:7465018E76649D630846F0F9BFDCB2C4DCF0B87D'],
+    key: '天宮花南'
+  },
+  {
+    name: 'ssni-976',
+    img: 'https://jp.netcdn.space/digital/video/ssni00976/ssni00976pl.jpg',
+    magnet: ['magnet:?xt=urn:btih:C85DBE2D925466A837D6C77FF94551E81A22E4FC'],
+    key: '山崎水愛'
+  },
+  {
+    name: 'STARS-411',
+    img: '',
+    magnet: ['magnet:?xt=urn:btih:819F94A35FBE05A5900C9F06DDB24BD6B582CF67'],
+    key: '天宮花南'
+  },
+  {
+    name: 'ssis-099',
+    img: 'https://www.98tuch.net/tupian/forum/202106/16/142111yiexgpngkp1ux19n.jpg',
+    magnet: ['magnet:?xt=urn:btih:4AD2D882FE78E52EEDCA77587C9B9577E2426176'],
+    key: '山崎水愛'
+  },
+  {
+    name: '青木小荷视频',
+    img: 'https://www.98tuch.net/tupian/forum/201908/28/100413kh327kibhlki23nd.jpg',
+    magnet: ['magnet:?xt=urn:btih:214970C8DE8DCE70BBBF2EB0084D6A1E65A4D1A8'],
+    key: '青木小荷'
+  },
+  {
+    name: 'MIDE-537',
+    img: 'https://www.98tuch.net/tupian/forum/201905/13/100154ezv6mij0cg694d34.jpg',
+    magnet: ['magnet:?xt=urn:btih:B3BCEFBAB745C2E42E954FFC44B67F2B46177679'],
+    key: '伊東ちなみ'
+  },
+  {
+    name: '3006635',
+    img: 'https://www.98tuch.net/tupian/forum/202207/02/091119ow6ktk6jckizitq8.jpg',
+    magnet: ['magnet:?xt=urn:btih:1E495753AB4F79C1CF9FC24AB094751E889D682B'],
+    key: 'FC2_1'
+  },
+  {
+    name: '3031937',
+    img: 'https://www.98tuch.net/tupian/forum/202207/09/085639vitynfbs8n3yovi3.jpg',
+    magnet: ['magnet:?xt=urn:btih:D89EE28676DC9F7F73CD1E2E8BDF36AFC7D4BC2B'],
+    key: 'FC2_1'
+  },
+  {
+    name: 'CWPBD-153',
+    img: '',
+    magnet: ['magnet:?xt=urn:btih:DF5CEAFAE3A0A6025AA0FD9DA8E658BBC441082F'],
+    key: '葵千恵'
+  },
+  {
+    name: '',
+    img: '',
+    magnet: [''],
+    key: ''
+  },
+]
